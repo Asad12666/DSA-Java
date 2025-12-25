ca6ea36ce3394e746486998e2529827993c5889e
Asad Muneer <asadmunirr321@gmail.com>
2025-12-25 13:08:20 +0500

    Test methods in TestClass

--- src/Stack/TestClass.java
+++ src/Stack/TestClass.java
@@ -7,6 +7,24 @@ public class TestClass {
         CustomDynamicStack<Double> stack2 = new CustomDynamicStack<>();
 
         stack.pushMultiple(1,2,3,4,5,6,7,8,9,10);
-        stack1.pushMultiple("Asad", "Majid", "Subaiyal", "Umaima", "Sadia", "Minal", "Zubair");
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println("Popped Element: "+ stack.pop());
+        System.out.println("Popped Element: "+ stack.pop());
+        System.out.println(stack+"\n\n");
+
+        stack1.pushMultiple("Asad", "Majid", "Subaiyal", "Abdullah", "Uzair", "Hammad", "Zubair");
+        System.out.println(stack1);
+        System.out.println(stack1.peek());
+        System.out.println("Popped Element: "+ stack1.pop());
+        System.out.println("Popped Element: "+ stack1.pop());
+        System.out.println(stack1+"\n\n");
+
+        stack2.pushMultiple(56.3, 67.9, 12.8, 78.4, 45.3, 12.9);
+        System.out.println(stack2);
+        System.out.println(stack2.peek());
+        System.out.println("Popped Element: "+ stack2.pop());
+        System.out.println("Popped Element: "+ stack2.pop());
+        System.out.println(stack2+"\n\n");
     }
 }
